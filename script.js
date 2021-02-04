b440ea999fb77418dfffb681602ddd479e79a1e8
Sarbjotm <sarbjotmann98@gmail.com>
2021-02-04 13:45:18 -0800

    Added Number Of Tasks Counter, Description, and changed Colours in css

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
+taskAmount = 0;
+
 function getTask(){
     var div = document.getElementById('toDoTasks')
     var task = document.getElementById('task').value;
-    div.innerHTML += task + "<br /> <br />"
-    document.getElementById('task').value = ''
+    var amount = document.getElementById('taskAmount')
+    if(task === ""){
+        alert("No Task to Insert")
+    }
+    else{
+        var para = document.createElement('p');
+        para.style.fontSize = '20px';
+        para.style.fontFamily = "Optima, sans-serif";
+        para.innerHTML = task + "<br /> <br />";
+        para.onclick = function(){
+            this.style.display = 'none'
+            taskAmount = taskAmount - 1;
+            amount.innerHTML = taskAmount;
+        }
+        document.querySelector("#toDoTasks").append(para);
+        document.getElementById('task').value = '';
+        taskAmount = taskAmount + 1;
+        amount.innerHTML = taskAmount;
+    }
+
     
 }
 
 
+
 function removeTask(){
+    taskAmount = 0;
+    var amount = document.getElementById('taskAmount').innerHTML = 0
     document.getElementById('task').value = ''
     document.getElementById('toDoTasks').innerHTML = ''
     
 }
-
-
-
