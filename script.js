ef882edb0fa5f05ba4c8ca4ad4fcbb7806d7e141
Sarbjotm <sarbjotmann98@gmail.com>
2021-02-05 01:01:27 -0800

    Added new theme

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 taskAmount = 0;
+theme = 0;
 
 function getTask(){
     var div = document.getElementById('toDoTasks')
@@ -27,11 +28,70 @@ function getTask(){
 }
 
 
-
 function removeTask(){
     taskAmount = 0;
     var amount = document.getElementById('taskAmount').innerHTML = 0
     document.getElementById('task').value = ''
     document.getElementById('toDoTasks').innerHTML = ''
     
+}
+
+function changeThemes(){
+    if(theme===0){
+        document.body.style.backgroundColor = "navy";
+        document.getElementById('titleTo').style.color = "lime";
+        document.getElementById('submitbtn').style.backgroundColor = "white";
+        document.getElementById('removebtn').style.backgroundColor = "lime";
+        document.getElementById('number').style.color = "lime"
+        document.getElementById('taskAmount').style.color = "lime"
+        theme = 1
+    }
+    else{
+        document.body.style.backgroundColor = "black";
+        document.getElementById('titleTo').style.color = "yellow";
+        document.getElementById('submitbtn').style.backgroundColor = "yellow"
+        document.getElementById('removebtn').style.backgroundColor = 'rgb(' + 250 + ',' + 103 + ',' + 255 + ')'
+        document.getElementById('number').style.color = "yellow"
+        document.getElementById('taskAmount').style.color = "yellow"
+        theme = 0
+    }
+}
+
+function hoverSubmit(){
+    if(theme === 0){
+        document.getElementById('submitbtn').style.backgroundColor = 'rgb(' + 247 + ',' + 211 + ',' + 51 + ')'
+    }
+    else{
+        document.getElementById('submitbtn').style.backgroundColor = "gray"
+    }
+
+}
+
+function hoverOutSubmit(){
+    if(theme === 0){
+        document.getElementById('submitbtn').style.backgroundColor = "yellow"
+    }
+    else{
+        document.getElementById('submitbtn').style.backgroundColor = "white"
+    }
+    
+}
+
+function hoverRemove(){
+    if(theme === 0){
+        document.getElementById('removebtn').style.backgroundColor = 'rgb(' + 209 + ',' + 157 + ',' + 211 + ')'
+    }
+    else{
+        document.getElementById('removebtn').style.backgroundColor = "green"
+    }
+}
+
+function hoverOutRemove(){
+    if(theme === 0){
+        document.getElementById('removebtn').style.backgroundColor = 'rgb(' + 250 + ',' + 103 + ',' + 255 + ')'
+    }
+    else{
+        document.getElementById('removebtn').style.backgroundColor = "lime"
+
+    }
 }
